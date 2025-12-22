d2f5a873b32c5a96de818cdd4cc71657ee3c5979
danyilispaniuk <danulispanyk@gmail.com>
2025-12-22 21:47:08 +0100

    lazy loading

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,20 @@
 import { Routes } from '@angular/router';
-import { MainPageComponent } from './pages/main/main.component';
 
 const routeConfig: Routes = [
     {
         path: '',
-        component: MainPageComponent, // The component to render for the path
-        title: 'Main page'
+        loadComponent: () =>
+        import('./pages/notes/notes.component')
+            .then(c => c.NotesComponent),
+        title: 'Notes'
     },
+    {
+        path: 'notepads',
+        loadComponent: () =>
+        import('./pages/notepads/notepads.component')
+            .then(c => c.NotepadsComponent),
+        title: 'Notepads'
+    }
     // You can add more routes here, for example:
     /*
     {
