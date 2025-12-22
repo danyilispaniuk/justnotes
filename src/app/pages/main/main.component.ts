52fc67e4bbe94caacd05d087a68d8f134135ec90
danyilispaniuk <danulispanyk@gmail.com>
2025-12-22 21:26:38 +0100

    Home page

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,11 +1,39 @@
 import { Component } from '@angular/core';
+import { NoteComponent } from '../../components/note/note.component';
+import { Note } from '../../services/interfaces/note';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-main',
-  imports: [],
+  imports: [CommonModule, NoteComponent],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainPageComponent {
-
+ noteList: Note[] = [
+    {
+      "id": 1,
+      "header": "First Note",
+      "notes": "This is the text for the first note.",
+      "created": new Date().toISOString()
+    },
+    {
+      "id": 2,
+      "header": "Second Note",
+      "notes": "This is another note.",
+      "created": new Date().toISOString()
+    },
+    {
+      "id": 3,
+      "header": "Third Note",
+      "notes": "And a third one for good measure.",
+      "created": new Date().toISOString()
+    },
+    {
+      "id": 4,
+      "header": "Fourth Note",
+      "notes": "Another one to fill the grid.",
+      "created": new Date().toISOString()
+    }
+  ]
 }
