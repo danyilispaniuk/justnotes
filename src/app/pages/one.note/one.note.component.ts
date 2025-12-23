84845bad96d84395e03e52354274d87cf0af1c58
danyilispaniuk <danulispanyk@gmail.com>
2025-12-23 21:29:45 +0100

    One note page logic & styling

--- src/app/pages/one.note/one.note.component.ts
+++ src/app/pages/one.note/one.note.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { NoteService } from '../../services/note.service';
 import { Note } from '../../services/interfaces/note';
 
 @Component({
@@ -8,12 +10,31 @@ import { Note } from '../../services/interfaces/note';
   styleUrl: './one.note.component.scss'
 })
 export class OneNoteComponent {
+  constructor(
+    private noteService: NoteService,
+    private route: ActivatedRoute
+  ){}
+
   isReadOnly: boolean = true
+
+  noteId!: string;
 
   note: Note = {
     id: 1,
     header: "testing",
     notes: "This is my new notes.",
     created: new Date().toISOString()
+  }
+
+  ngOnInit(): void{
+    console.log(this.note)
+    this.route.paramMap.subscribe(params => {
+        this.noteId = params.get('id')!;
+        console.log('Note ID:', this.noteId);
+        this.noteService.getNote(this.noteId).subscribe(response => {
+          console.log(response);
+          this.note = response;
+        });
+    });
   }
 }
